f8e25973af265029a7cd6ea80ff2ec676717319a
Kavya Raghavendran <kavyaraghavendran10@gmail.com>
2026-09-03 22:21:56 +0530

    docker

---
+++ Docker/java-app/Main.java
@@ -0,0 +1,22 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "Hello World from Java!";
+            exchange.sendResponseHeaders(200, response.length());
+
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
+
+        server.start();
+        System.out.println("Java server running on port 8080");
+    }
+}
